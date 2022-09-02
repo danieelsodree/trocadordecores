82b4e405381483009c14746f7a879271e66781ee
Daniel Sodré <danielcsodre98@gmail.com>
2022-09-01 21:13:47 -0300

    adicionado btn reset

--- script4.js
+++ script4.js
@@ -2,6 +2,7 @@ let num = [1, 2, 3]
 let btnRed = document.getElementById("btn-red");
 let btnBlue = document.getElementById("btn-blue");
 let btnGreen = document.getElementById("btn-green");
+let btnReset = document.getElementById("btn-reset")
 let titulo = document.getElementById("titulo");
 let container = document.getElementById("container")
 
@@ -9,6 +10,7 @@ function loaded(){
     btnRed.addEventListener("click", trocaRed)
     btnBlue.addEventListener("click", trocaBlue)
     btnGreen.addEventListener("click", trocaGreen)
+    btnReset.addEventListener("click", trocaReset)
 }
 
 function trocaRed(){
@@ -26,16 +28,7 @@ function trocaGreen(){
     container = document.getElementById("container").style.backgroundColor = "green"
 }
 
-function troca(){
-    let math = Math.floor(Math.random() * 3)
-    if(math == 0){
-        titulo.innerText = "RED"
-        container = document.getElementById("container").style.backgroundColor = "red";
-    } else if ( math == 2){
-        titulo.innerText = "BLUE"
-        container = document.getElementById("container").style.backgroundColor = "blue";
-    } else {
-        titulo.innerText = "GREEN"
-        container = document.getElementById("container").style.backgroundColor = "green";
-    }
+function trocaReset(){
+    titulo.innerText = "Change The Color";
+    container = document.getElementById("container").style.backgroundColor = "white";
 }
